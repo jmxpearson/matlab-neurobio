781458f97a9699831e3f9382a6e7da13d9a33021
John Pearson <jmxpearson@gmail.com>
2017-01-12 11:25:54 -0500

    whoops

--- week1/class1.m
+++ week1/class1.m
@@ -32,27 +32,3 @@ grpstats(tidy, 'Epoch', 'mean', 'DataVars', 'Count')
 % boxplot
 boxplot(tidy.Count, tidy.Epoch, 'notch', 'on')  % not particularly illuminating
 
-% restrict data to choice trials and reward outcome period
-subset = (tidy.cued) == 0 & (tidy.Epoch == 'RewardOn_sp_count');
-sub_tidy = tidy(subset, :);
-
-% by reward type
-grpstats(sub_tidy, 'outcome', 'mean', 'DataVars', 'Count')
-boxplot(sub_tidy.Count, sub_tidy.outcome)
-
-% by reward magnitude
-grpstats(sub_tidy, 'reward', 'mean', 'DataVars', 'Count')
-boxplot(sub_tidy.Count, sub_tidy.reward)
-
-% make prosocial choice column
-sub_tidy.prosocial = sub_tidy.outcome == 'other' | sub_tidy.outcome == 'both';
-
-% more group statistics
-grpstats(sub_tidy, 'subj', 'mean', 'DataVars', 'prosocial')
-grpstats(sub_tidy, {'subj', 'reward'}, 'mean', 'DataVars', 'prosocial')
-
-% a glm fit and a fit for a model with just a constant
-% we can compare these with AIC or BIC
-fit = fitglm(sub_tidy, 'prosocial~subj*reward', 'Distribution', 'binomial');
-nullfit = fitglm(sub_tidy, 'prosocial~1', 'Distribution', 'binomial');
-
