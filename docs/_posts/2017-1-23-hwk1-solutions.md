6b2d14e0c8cabbf5f4fc272c5c05b9276f4e8bf0
John Pearson <jmxpearson@gmail.com>
2017-01-23 10:59:04 -0500

    adding post about solutions

---
+++ docs/_posts/2017-1-23-hwk1-solutions.md
@@ -0,0 +1,9 @@
+---
+layout: default
+title: Quantitative Neurobiology
+desc: Matlab notes, assignments, and code
+post_title: Homework 1 solutions
+author: John Pearson
+category: blog
+---
+Solutions to [Homework 1](http://jmxpearson.com/matlab-neurobio/exercises/week-1-homework-split-apply-combine.html) are now available ([link](https://github.com/jmxpearson/matlab-neurobio/blob/master/week1/hwk1.m)). 
---
+++ week1/hwk1.m
@@ -0,0 +1,40 @@
+dat = readtable('../data/week1/ppv_data.csv');
+
+catvars = {'monk', 'session', 'piccat'};
+for idx=1:length(catvars)
+    cv = catvars{idx};
+    dat.(cv) = categorical(dat.(cv));
+end
+
+% subset data
+session = '9';
+piccat = '4';
+subset = dat((dat.session == session) & (dat.piccat == piccat), :);
+subset = subset(subset.Ntot ~= 0, :);
+
+% plot data
+subset.proportion = subset.Nimg./subset.Ntot;
+scatter(subset.dv, subset.proportion);
+
+% do fit
+fit = fitglm(subset, 'Nimg ~ dv', 'Distribution', 'binomial', 'BinomialSize', subset.Ntot);
+coeff_table = fit.Coefficients;
+beta = coeff_table.Estimate;
+
+% plot fit
+dv = (-150:1:150)';
+new_vals = table(dv);
+p_pred = predict(fit, new_vals);
+hold on
+plot(dv, p_pred);
+
+% calculate image value
+img_val = -beta(1)/beta(2);
+p_slope = coeff_table.pValue(2);
+
+% see fit_table.m for function
+[imvals, pvals, sessions, categories] = splitapply(@fit_model, dat, findgroups(dat.session, dat.piccat));
+mod_tbl = table(imvals, pvals, sessions, categories);
+
+bads = abs(mod_tbl.imvals) > 1000;
+mod_tbl(bads, :) = [];
