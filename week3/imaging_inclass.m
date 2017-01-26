eff1bbbd1ceaedf797bc07fe81b22e6e7a5dc916
John Pearson <jmxpearson@gmail.com>
2017-01-25 22:04:25 -0500

    solutions for 3.1

---
+++ week3/imaging_inclass.m
@@ -0,0 +1,76 @@
+load '../data/week3/DirectionTuning_V1_dec.mat'
+
+%% how do we figure out what the dimensions are?
+% what would make sense for memory locality?
+% what test would you do (visually, computationally) to check your answer?
+% let's try to plot
+imagesc(data(:, :, 1))
+colorbar()
+figure
+imagesc(data(:, :, 5))
+colorbar()
+% note that these look similar, which is what we expect if only separated
+% in time
+
+[Nvert, Nhorz, Nframes] = size(data);
+    
+%% let's look at a time series
+% frames are acquired at 30 Hz, downsampled to 10 Hz
+dt = 1/3;
+taxis = dt:dt:size(data, 3)*dt;
+clf
+hold all
+plot(taxis, squeeze(data(55, 125, :)))
+plot(taxis, squeeze(data(75, 180, :)))
+
+%% show a "movie"
+% adapted from `doc movie`
+figure
+
+% normalizer:
+pmax = max(data(:));
+pmin = min(data(:));
+
+for tt = 1:Nframes
+    imagesc((data(:, :, tt) - pmin)/(pmax - pmin))
+    drawnow
+end
+
+
+%% let's plot the time series for one pixel
+pt_in_img = [46, 200];
+tseries = squeeze(data(pt_in_img(1), pt_in_img(2), :));
+plot(taxis, tseries)
+
+%% ...and figure out its tuning
+% Every orientation is off for 12, on for 6
+% strategy: make a grouped variable for each frame, then get a group mean
+% for each condition
+labels = -1 * ones(length(taxis), 1); % -1 will label baseline (off)
+codes = dirTuningExp.tGratingDirectionDeg;
+Ntrials = length(codes);
+Noff = dirTuningExp.stimOffFrames;
+Non = dirTuningExp.stimOnFrames;
+
+offset = 0;
+for tt = 1:Ntrials
+    offset = offset + Noff;
+    labels(offset + (1:Non)) = codes{tt};
+    offset = offset + Non;
+end
+labels = categorical(labels);
+orientations = categories(labels);
+
+tuning = grpstats(tseries, labels);
+
+figure
+plot(tuning(2:end))
+xticklabels(orientations(2:end))
+refline(0, tuning(1))
+%% a really brittle way to do tuning is to find the max absolute response
+% relative to baseline; assumes tuning is always an *increase* above
+% baseline
+tcurve = tuning(2:end) - tuning(1);
+[~, peakind] = max(tcurve);
+
+preferred = orientations{peakind};
